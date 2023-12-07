dd1785d2fcac0cd8837ba72c38e50d2beef40987
Nathan <59203078+Jenavla@users.noreply.github.com>
2023-12-07 14:36:29 +0100

    Fitvergelijking code.py

--- Fitvergelijking code.py
+++ Fitvergelijking code.py
@@ -15,11 +15,11 @@ def fit_vergelijking(n_glas, d, i, r, lapda):
     return term1 * (term2 + term3 - term4 - term5 - term6)
 
 # Constanten
-n_glas = 1.5
+n_glas = 1.49
 n_lucht = 1.00029
-d = 2e-3
+d = 3e-3
 lapda = 532e-9
-i = np.radians(np.linspace(0, 10, 10))
+i = np.radians(np.linspace(0, 15, 1000))
 r = np.arcsin(((n_lucht * np.sin(i)) / n_glas))
 
 # Bereken het aantal fringes op basis van de functie
@@ -30,6 +30,6 @@ plt.plot(np.degrees(i), aantal_fringes)
 plt.xlabel('Invalshoek (graden)')
 plt.ylabel('Aantal fringes')
 plt.title('Plot van Fit Vergelijking')
-plt.ylim(0, 40)
-plt.xlim(0, 10)
+plt.ylim()
+plt.xlim(0, 15)
 plt.show()
