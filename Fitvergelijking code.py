c29f650eb60edd9ecf88b5b851396d7b87460541
Nathan <59203078+Jenavla@users.noreply.github.com>
2023-12-10 13:03:47 +0100

    Fitvergelijking code.py

--- Fitvergelijking code.py
+++ Fitvergelijking code.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sp
 
 # Definieer de functie op basis van de gegeven formule
 def fit_vergelijking(n_glas, d, i, r, lapda):
@@ -25,11 +27,24 @@ r = np.arcsin(((n_lucht * np.sin(i)) / n_glas))
 # Bereken het aantal fringes op basis van de functie
 aantal_fringes = fit_vergelijking(n_glas, d, i, r, lapda) 
 
+# gemeten gegevens
+i_nieuw = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+fringes = np.array([0, 0, 10, 13, 16, 22, 27, 29, 29, 33, 12, 10, 11, 17, 21, 25, 32, 31, 34, 34])
+
+# Plot de resultaten
+plt.plot(np.degrees(i), aantal_fringes, label='Theorie')
+plt.scatter(i_nieuw,fringes, c='red', marker='x', label='Praktijk')
+plt.xlabel('Invalshoek (graden)')
+plt.ylabel('Aantal fringes')
+plt.title('Plot van Fit Vergelijking')
+plt.legend()
+plt.show()
+
 # Plot de resultaten
 plt.plot(np.degrees(i), aantal_fringes)
 plt.xlabel('Invalshoek (graden)')
 plt.ylabel('Aantal fringes')
 plt.title('Plot van Fit Vergelijking')
 plt.ylim()
-plt.xlim(0, 15)
+plt.xlim(0, 10)
 plt.show()
