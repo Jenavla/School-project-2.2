10f3c189d67c73fd8cfc914d6c6553dc676d8b04
Nathan <59203078+Jenavla@users.noreply.github.com>
2023-12-18 11:05:07 +0100

    Fitvergelijking code.py

--- Fitvergelijking code.py
+++ Fitvergelijking code.py
@@ -1,10 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import pandas as pd
-import sympy as sp
+
+data = pd.read_csv('datacsv.csv', sep=',')
+
+# Constanten
+n_glas = 1.49
+n_lucht = 1.00029
+d = 3e-3
+lapda = 532e-9
+i_theorie = np.radians(np.linspace(0, 15, 1000))
+
+# Datareading
+column_names = ['hoeken', 'franjes'] 
+data.columns = column_names
+i_praktijk = np.radians(data['hoeken'])
+fringes_praktijk = data['franjes']
+
+# Combineer hoeken en fringes in een dictionary voor gemakkelijke verwerking
+data_dict = {}
+for hoek, fringes in zip(i_praktijk, fringes_praktijk):
+    if hoek not in data_dict:
+        data_dict[hoek] = [fringes]
+    else:
+        data_dict[hoek].append(fringes)
+
+# Bereken het gemiddelde aantal fringes voor elke unieke hoek
+unieke_hoeken = np.array(list(data_dict.keys()))
+gemiddelde_fringes = np.array([np.mean(data_dict[hoek]) for hoek in unieke_hoeken])
 
 # Definieer de functie op basis van de gegeven formule
-def fit_vergelijking(n_glas, d, i, r, lapda):
+def theoretische_fringes(i, n_glas, d, lapda):
+    r = np.arcsin(((n_lucht * np.sin(i)) / n_glas))
     term1 = (2 * d) / lapda
     term2 = n_glas / np.cos(r)
     term3 = np.tan(i) * np.sin(i)
@@ -12,39 +40,41 @@ def fit_vergelijking(n_glas, d, i, r, lapda):
     term5 = (n_glas - 1)
     term6 = 1 / np.cos(i)
     
-    print(np.max(term1 * (term2 + term3 - term4 - term5 - term6)))
-    
     return term1 * (term2 + term3 - term4 - term5 - term6)
 
-# Constanten
-n_glas = 1.49
-n_lucht = 1.00029
-d = 3e-3
-lapda = 532e-9
-i = np.radians(np.linspace(0, 15, 1000))
-r = np.arcsin(((n_lucht * np.sin(i)) / n_glas))
+# Definieer de functie voor de fit op basis van praktijkmetingen
+def fit_func(i, n):
+    return theoretische_fringes(i, n, d, lapda)
 
-# Bereken het aantal fringes op basis van de functie
-aantal_fringes = fit_vergelijking(n_glas, d, i, r, lapda) 
+# Bereken de theoretische waarden voor de hele reeks invalshoeken
+theoretische_waarden = theoretische_fringes(i_theorie, n_glas, d, lapda)
 
-# gemeten gegevens
-i_nieuw = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-fringes = np.array([0, 0, 10, 13, 16, 22, 27, 29, 29, 33, 12, 10, 11, 17, 21, 25, 32, 31, 34, 34])
+# Pas de brekingsindex toe op basis van praktijkmetingen
+brekingsindex, a = curve_fit(fit_func, i_praktijk, fringes_praktijk)
 
-# Plot de resultaten
-plt.plot(np.degrees(i), aantal_fringes, label='Theorie')
-plt.scatter(i_nieuw,fringes, c='red', marker='x', label='Praktijk')
+# Haal de standaarddeviaties uit de covariantiematrix
+std_dev = np.sqrt(np.diag(a))
+
+# Bereken het 95% betrouwbaarheidsinterval
+bbi = 1.96 * std_dev 
+
+# Bereken de standaardfout
+standaard_fout = std_dev[0] / np.sqrt(len(i_praktijk))
+
+# Bereken de praktische waarden voor de invalshoeken
+praktijk_waarden = theoretische_fringes(unieke_hoeken, brekingsindex, d, lapda)
+
+# Plot de resultaten met foutenbalken
+plt.scatter(np.degrees(unieke_hoeken), gemiddelde_fringes, c='red', marker='x', label='Praktijk')
+plt.plot(np.degrees(i_theorie), theoretische_waarden, label='Theorie')
+plt.plot(np.degrees(unieke_hoeken), praktijk_waarden, c='red')
 plt.xlabel('Invalshoek (graden)')
 plt.ylabel('Aantal fringes')
-plt.title('Plot van Fit Vergelijking')
+plt.title('Plot van Fit Vergelijking met Betrouwbaarheidsinterval')
 plt.legend()
 plt.show()
 
-# Plot de resultaten
-plt.plot(np.degrees(i), aantal_fringes)
-plt.xlabel('Invalshoek (graden)')
-plt.ylabel('Aantal fringes')
-plt.title('Plot van Fit Vergelijking')
-plt.ylim()
-plt.xlim(0, 10)
-plt.show()
+print(f"Berekende brekingsindex n: {brekingsindex[0]}")
+print(f"Standaarddeviatie: {std_dev[0]}")
+print(f"Betrouwbaarheidsinterval (95%): {bbi[0]}")
+print(f"Standaardfout: {standaard_fout}")
